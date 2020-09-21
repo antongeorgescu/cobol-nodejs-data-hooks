3b6652072f726a03c07cab9799a9e9e60b636e17
Anton Georgesco <antongeorgescu@gmail.com>
2020-09-21 17:53:03 -0400

    added green screen components

---
+++ green-screen/data-access-layer/StudentRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+namespace data_access_layer
+{
+    public class StudentRecord
+    {
+        public string StudentId { get; set; }
+        public string FullName { get; set; }
+        public string DOB { get; set; }
+        public string ProgramCode { get; set; }
+        public char Gender { get; set; }
+        public string PhoneNo { get; set; }
+        public int LoanAmount { get; set; }
+    }
+}
---
+++ green-screen/data-access-layer/StudentRecords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_access_layer
+{
+    public class StudentRecords
+    {
+        List<StudentRecord> allRecords;
+
+        public List<StudentRecord> GetAllRecords() {
+            return null;
+        }
+         
+        public void AddRecord(StudentRecord aRecord)
+        {
+            allRecords.Add(aRecord);
+        }
+
+        public void UpdateRecord(string studentId, StudentRecord aRecord)
+        {
+            return;
+
+        }
+    }
+}
